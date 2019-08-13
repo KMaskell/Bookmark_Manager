3f0c594dc5dcb50c001ba8e12a561c029f5f9e28
Kat <vampirebatkat@gmail.com>
2019-08-13 15:51:53 +0100

    task 9 passing

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ I would like to see a list of bookmarks
 - Connect to the database using the pqsl command `\c bookmark_manager;`
 
 - Run the query we have saved in the file `01_create_bookmarks_table.sql`
+
+
+Connect to `psql` and create the `bookmark_manager` and `bookmark_manager_test` databases:
+
+CREATE DATABASE bookmark_manager;
+CREATE DATABASE bookmark_manager_test;
+
+To set up the appropriate tables, connect to each database in `psql` and run the SQL scripts in the `db/migrations` folder in the given order.
+
+
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,12 +1,20 @@
 require 'bookmark'
+require 'pg'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all bookmarks' do
+    it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.lwlies.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.ocado.com');")
+
       bookmarks = Bookmark.all
-      expect(bookmarks).to include"http://www.google.com"
-      expect(bookmarks).to include"http://www.lwlies.com"
-      expect(bookmarks).to include"http://www.ocado.com"
+
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.lwlies.com')
+      expect(bookmarks).to include('http://www.ocado.com')
     end
   end
 end
